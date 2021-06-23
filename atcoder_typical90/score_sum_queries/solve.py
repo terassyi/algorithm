fab5fa348b4e6db1eaf8ff1ac5e9726badfe2bca
terassyi <iscale821@gmail.com>
2021-06-23 23:41:57 +0900

    fix 010

--- atcoder_typical90/score_sum_queries/solve.py
+++ atcoder_typical90/score_sum_queries/solve.py
@@ -5,12 +5,19 @@ c_p = [list(map(int, input().split())) for i in range(n)]
 q = int(input())
 l_r = [list(map(int, input().split())) for i in range(q)]
 
+a_b = []
+a, b = 0, 0
+for c, p in c_p:
+	if c == 1:
+		a += p
+	else:
+		b += p
+	a_b.append([a, b])
+
 for l, r in l_r:
-	a, b = 0, 0
-	for i in range(l-1, r):
-		c, p = c_p[i]
-		if c == 1:
-			a += p
-		else:
-			b += p
+	if l == 1:
+		print("{0} {1}".format(a_b[r-1][0], a_b[r-1][1]))
+		continue
+	a = a_b[r-1][0] - a_b[l-2][0]
+	b = a_b[r-1][1] - a_b[l-2][1]
 	print("{0} {1}".format(a, b))
